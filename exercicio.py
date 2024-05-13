54877891bb85804c43a519de0113e6b34b21337d
wwwspmarcosvinicius1234 <167825799+wwwspmarcosvinicius1234@users.noreply.github.com>
2024-05-13 20:49:59 -0300

    Add files via upload

---
+++ exercicio.py
@@ -0,0 +1,9 @@
+#Marcos vinicius rodrigues ramos
+numero = int(input())
+antecessor =numero -1
+sucessor=numero +1
+print("o antecessor de",numero, "e",antecessor)
+print("o sucessor de",numero,"e",sucessor)
+ 
+    
+
