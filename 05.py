60e426f47c7e52898cc0117c341b3dfe9971829b
WilliamSoares01 <167824982+WilliamSoares01@users.noreply.github.com>
2024-05-14 20:50:57 -0300

    Add files via upload

---
+++ 05.py
@@ -0,0 +1,12 @@
+#Condições 
+n = int(input("Insira um número: "))
+
+if n % 5 == 0 : 
+    print("Número dívisivel por 5")
+else:
+    print("Número não dívisivel por 5")
+
+    
+          
+
+    
