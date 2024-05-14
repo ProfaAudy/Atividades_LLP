58ccee7b3f18718ba223caa9a9ef03713a2ee4d6
ERIKA-048 <167824965+ERIKA-048@users.noreply.github.com>
2024-05-14 20:22:23 -0300

    14/05

---
+++ atividade.py
@@ -0,0 +1,10 @@
+# AULA DO DIA 14/05
+
+#ATIVIDADE
+
+numero = int(input("digite um número "))
+if  numero % 5 == 0:
+    print("esse numero é divisivel por 5")
+else:
+    print("não é divisivel")
+
