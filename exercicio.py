d54c0373238d7cece62a0b3bcaf75ef09bc4f097
dandaramisray2024 <167824220+dandaramisray2024@users.noreply.github.com>
2024-05-14 21:15:00 -0300

    Add files via upload

---
+++ exercicio.py
@@ -0,0 +1,13 @@
+print ("Hello World")
+#"hello world"
+#numero =int(input("insira um numero para informar se ele é divisivel por 5"))
+#print (numero/5)
+#numero =int(input("insira o numero que sera dividido"))
+
+#condições
+n=int(input("insira um numero: "))
+# divida por 5
+if n  % 5 == 0:
+    print ("o numero {numero} é divisivel por cinco")
+else:
+    print("o numero {numero} não é divisivel por 5")
