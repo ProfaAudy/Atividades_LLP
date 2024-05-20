c545be24c77cf437e3a28cfeb7cd28db3db41579
xLuanznx <167824222+xLuanznx@users.noreply.github.com>
2024-05-20 20:20:01 -0300

    Add files via upload

---
+++ Aula 1.py
@@ -0,0 +1,6 @@
+print("hello wolrd")
+#"comentario"
+print("Para saber o antecessor e o sucessor de um número")
+numero = int(input("insira o número aqui:"))
+print("sucessor:",numero+1)
+print("antecessor:",numero+2)
