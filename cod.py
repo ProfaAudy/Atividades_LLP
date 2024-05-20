8d77a1de069c05b1c9b3da400f7068f53f796f89
edu8364 <167824167+edu8364@users.noreply.github.com>
2024-05-20 19:39:30 -0300

    Add files via upload

---
+++ cod.py
@@ -0,0 +1,39 @@
+#print("Hello World")
+#"Hello world"
+#numero = int(input(Insira um número:))
+#print(numero*2)
+#print(numero+2)
+#print(numero-2)
+#print(numero/2)
+#print(numero%2)
+
+#Condições
+#n = int(input("Insira uma idade"))
+# > maior que
+#if n >= 60:
+#    print("Pessoa idosa")
+#elif n >= 18:
+#    print("Maior de idade")
+#else:
+#    print("Menor de idade")
+#if n == 55*48:
+#    print("igual")
+#if n != 40:
+#    print("Diferente")
+#if n >= 45:
+#    print("Maior ou igual")
+#if n <= 35*3:
+#    print("Menor ou igual")
+#if n > 78:
+#    print("Maior")
+#if n < 100:
+#    print("Menor")
+print("Insira um número")
+n= int(input())
+
+if n < 4 and n > 1:
+    print("Número menor que 4 E maior que1")
+if n >= 10 or n >= 5:
+    print("Número maior ou igual a 10 OU maior ou igual a 5")
+if not n % 2 != 1:
+    print("Número NÃO ímpar")
