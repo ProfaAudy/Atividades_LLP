cc33f16559e836662113bb14c617c25da4c3f512
ProfaAudy <167873774+ProfaAudy@users.noreply.github.com>
2024-05-20 20:13:35 -0300

    Delete atividade.py

--- atividade.py
+++
@@ -1,7 +0,0 @@
-numero = int(input("digite um numero"))
-
-# verifica se o numero e divisivel por cinco
-if numero % 5 == 0:
-    print("o numero e divisivel por cinco")
-else:
-    print("o numero não e divisivel por cinco")
