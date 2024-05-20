7c2f0b97a5d54a19d55b7548c458d652d3203606
Kauealves7 <167824377+Kauealves7@users.noreply.github.com>
2024-05-20 19:30:59 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,11 @@
+# Recebe as quatro notas do usuário
+nota1 = int(input("Digite a primeira nota: "))
+nota2 = int(input("Digite a segunda nota: "))
+nota3 = int(input("Digite a terceira nota: "))
+nota4 = int(input("Digite a quarta nota: "))
+
+# Calcula a média aritmética
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+# Exibe o resultado
+print("A média aritmética é:", media)
