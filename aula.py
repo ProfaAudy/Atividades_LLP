f766cb41d177088e7744429dcf97341842e5b89d
camilla2911 <167824352+camilla2911@users.noreply.github.com>
2024-05-21 18:55:55 -0300

    Numeros Antecessor e Sucessor

---
+++ aula.py
@@ -0,0 +1,8 @@
+number = int(input("Enter an integer: "))
+predecessor = number - 1
+successor = number + 1
+ 
+print("\n")
+print("The predecessor is:", predecessor)
+print("The number is:", number)
+print("The successor is:", successor)
