ec0492d298182782a53e88e13753868ebe5fd7fa
WilliamSoares01 <167824982+WilliamSoares01@users.noreply.github.com>
2024-05-21 18:36:17 -0300

    Add files via upload

---
+++ atividade2.py
@@ -0,0 +1,12 @@
+#Le o número do usuario
+
+número = int(input( "Insira um número:"))
+
+#Calcular o antecessor e o sucessor 
+antecessor = número -1 
+sucessor = número +1
+
+#Apresentar o antecessor e o sucessor 
+print("Antecessor", {antecessor})
+print("Sucessor", {sucessor})
+        
