0d6d430ce5475bd39e9f15c288f425c52ae2623e
Vagner9383 <167824478+Vagner9383@users.noreply.github.com>
2024-05-24 18:57:13 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,4 @@
+numero = int(input("digite um número "))
+if numero % 5 == 0:
+    print("esse numero e divisivel por 5")
+else:print("não e divisivel")
---
+++ atividade6.py
@@ -0,0 +1,4 @@
+numero = int(input("insira um número: "))
+for i in range(1,11):
+  produto= numero * i
+  print (produto)
