e496fc62993cad3e1286d24ca4ddabfd0b7a3342
dandaramisray2024 <167824220+dandaramisray2024@users.noreply.github.com>
2024-05-24 18:35:15 -0300

    Add files via upload

---
+++ exercicio2.py
@@ -0,0 +1,12 @@
+print ("Hello World")
+#"Hello World"
+#numero=int(input("insira o numero para saber o seu peso"))
+
+#condições
+n=int(input("insira um numero"))
+# > maior que
+if n >= 24:
+    print (" {numero} sobrepeso ou obesidade de 1,2 e 3 grau ")
+# < menor que
+elif n <= 18:
+    print ("{numero} é abaixo do peso")
