95979967dc294145518f1ff1972c8c69e96a9407
victordasilvaporto <167824499+victordasilvaporto@users.noreply.github.com>
2024-05-28 20:50:51 -0300

    Victor da silva porto (#10)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload

---
+++ att.py
@@ -0,0 +1,4 @@
+numero = int(input("digite um número"))
+if numero % 5 == 0:
+    print("divisivel")
+else: print("nao divisivel")
---
+++ numero.py
@@ -0,0 +1,6 @@
+n = int(input("insira um numero"))
+antecessor = n - 1
+sucessor = n + 1 
+print (antecessor)
+print (sucessor)
+print ("antecessor e sucessor")
